b781b0e693c91bc19e211275807896b63b7fbf26
Kevin Martins <kevinmmartins@gmail.com>
2018-09-05 22:25:17 -0300

    Created mongo database

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const parser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
 
 const apiRouter = require('./routes/api-route');
-const thingProviderRouter = require('./routes/thing-provider-route')
+const thingProviderRouter = require('./routes/thing-provider-route');
+
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds245772.mlab.com:45772/thingprovider');
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: false }));
