e09bb7327734407532a7c4cefeb18b0f489875f7
Kevin Martins <kevinmmartins@gmail.com>
2018-09-29 20:16:44 -0300

    Fix mongo db warnings

--- src/app.js
+++ src/app.js
@@ -6,8 +6,9 @@ const mongoose = require('mongoose');
 const app = express();
 const config = require('../bin/config');
 
-mongoose.connect('mongodb://' + config.storageConfig.database + ':' + config.storageConfig.pass + '@ds245772.mlab.com:45772/thingprovider');
-const Service =require('./models/service-route');
+mongoose.set('useCreateIndex', true);
+mongoose.connect('mongodb://' + config.storageConfig.database + ':' + config.storageConfig.pass + '@ds245772.mlab.com:45772/thingprovider', { useNewUrlParser: true });
+const Service = require('./models/service-route');
 
 const apiRouter = require('./routes/api-route');
 const thingProviderRouter = require('./routes/thing-provider-route');
