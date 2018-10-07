e91fcc4a866415d154848ffdfd21739f32d1a54c
Kevin Martins <kevinmmartins@gmail.com>
2018-10-07 00:12:30 -0300

    Add cors filter

--- src/app.js
+++ src/app.js
@@ -3,21 +3,22 @@
 const express = require('express');
 const parser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 const config = require('../bin/config');
 
 mongoose.set('useFindAndModify',false);
 mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://' + config.storageConfig.database + ':' + config.storageConfig.pass + '@ds245772.mlab.com:45772/thingprovider', { useNewUrlParser: true });
-const Service = require('./models/service-route');
+require('./models/service-route');
 
 const apiRouter = require('./routes/api-route');
 const thingProviderRouter = require('./routes/thing-provider-route');
 const serviceRouter = require('./routes/thing-to-service-route');
 
+app.use(cors());
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: false }));
-
 app.use('/tps/version', apiRouter);
 app.use('/tps/', thingProviderRouter);
 app.use('/route/', serviceRouter);
